74e33ca08a4758381248298d79da66adac5220f7
onuroner <onuroner4054@gmail.com>
2022-05-02 13:06:09 +0300

    first commit

---
+++ BlazorLeaflet/BlazorLeaflet.Samples/Data/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorLeaflet.Samples.Data
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Workers { get; set; }
+        public string Description { get; set; }
+        public int Capacity { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
---
+++ BlazorLeaflet/BlazorLeaflet.Samples/Data/ProductDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorLeaflet.Samples.Data
+{
+    public class ProductDbContext : DbContext
+    {
+        #region Contructor
+        public ProductDbContext(DbContextOptions<ProductDbContext> options)
+                : base(options)
+        {
+
+        }
+        #endregion
+
+        #region Public properties
+        public DbSet<Product> Product { get; set; }
+        #endregion
+
+        #region Overidden methods
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(GetProducts());
+            base.OnModelCreating(modelBuilder);
+        }
+        #endregion
+
+
+        #region Private methods
+        private List<Product> GetProducts()
+        {
+            return new List<Product>
+    {
+       
+    };
+        }
+        #endregion
+    }
+}
